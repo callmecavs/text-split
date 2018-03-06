ad3a58765356507fdcf8087deb1c8a039835a591
Michael Cavalea <callmecavs@gmail.com>
2018-03-05 19:35:43 -0500

    working

--- src/text-split.js
+++ src/text-split.js
@@ -3,19 +3,41 @@ const separators = {
   word: ' '
 }
 
-const split = (node, {
+const split = (target, {
   // default options
-  a11y = false,
+  a11y = true,
   delimeter = 'letter',
   element = 'span',
-  each = () => {}
+  each = null
 } = {}) => {
   // split text based on delimeter
-  const text = node
+  const text = target
     .textContent
     .split(separators[delimeter])
 
-  console.log(text)
+  // create an element for each piece of text
+  // the element and index are passed to the each callback if it exists
+  const frag = document.createDocumentFragment()
+  const len = text.length
+
+  let index
+  let node
+
+  for (index = 0; index < len; index++) {
+    node = document.createElement(element)
+    node.textContent = text[index]
+    a11y && node.setAttribute('aria-hidden', 'true')
+    each && each(node, index)
+    frag.appendChild(node)
+  }
+
+  // append label to target
+  // must be done before clearing target.innerHTML
+  if (a11y) target.setAttribute('aria-label', target.textContent)
+
+  // clear text, and append fragment
+  target.innerHTML = ''
+  target.appendChild(frag)
 }
 
 export default split
