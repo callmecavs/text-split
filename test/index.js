37cf7f847a2dc1baf5f78b000bdc90e35374a249
Michael Cavalea <callmecavs@gmail.com>
2018-03-05 20:31:34 -0500

    add split by letter etst

--- test/index.js
+++ test/index.js
@@ -1,9 +1,30 @@
+require('jsdom-global')()
+
 const expect = require('chai').expect
+const split = require('../dist/text-split.js')
 
 /* eslint-env mocha */
 
 describe('text-split', () => {
-  it('should have tests', () => {
-    expect(true).to.equal(true)
+  it('should split by letter', () => {
+    const test = document.createElement('p')
+    test.textContent = 'Split text.'
+    split(test)
+
+    const expected = [
+      '<span aria-hidden="true">S</span>',
+      '<span aria-hidden="true">p</span>',
+      '<span aria-hidden="true">l</span>',
+      '<span aria-hidden="true">i</span>',
+      '<span aria-hidden="true">t</span>',
+      '<span aria-hidden="true"> </span>',
+      '<span aria-hidden="true">t</span>',
+      '<span aria-hidden="true">e</span>',
+      '<span aria-hidden="true">x</span>',
+      '<span aria-hidden="true">t</span>',
+      '<span aria-hidden="true">.</span>'
+    ].join('')
+
+    expect(test.innerHTML).to.equal(expected)
   })
 })
